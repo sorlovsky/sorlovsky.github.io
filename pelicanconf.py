7c6ff9b4fff75dca42200d1d777ce58c07bda633
sorlovsky <simon.orlovsky@gmail.com>
2017-08-28 21:57:18 -0600

    Added stuff

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,6 +7,9 @@ SITENAME = u'Piegenstuff'
 SITEURL = ''
 
 PATH = 'content'
+SITESUBTITLE ='Mostly python and math stuff'
+SITETAG = "Piegenstuff"
+
 
 TIMEZONE = 'America/Los_Angeles'    
 
@@ -23,9 +26,21 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Dormcuts in USA Today', 'http://college.usatoday.com/2015/10/17/dorm-hair-cuts/'),
          ('Dancing with the Stars', 'https://www.youtube.com/watch?v=Dpy_5Hy0zv8'))
 
-# Social widget
-SOCIAL = (('Instagram', 'http://instagram.com/simonorlovsky'),
-          ('LinkedIn', 'http://linkedin.com/in/simonorlovsky'),)
+# Put taglist at end of articles, and use the default sharing button implementation.
+CUSTOM_ARTICLE_FOOTERS = ("taglist.html", "sharing.html", )
+CUSTOM_SCRIPTS_ARTICLE = "sharing_scripts.html"
+
+# Default sidebar template. Omit this setting for single column mode without sidebar.
+SIDEBAR = "sidebar.html"
+
+SOCIAL = (('Twitter', 'https://twitter.com/simonthebarber',
+         'fa fa-twitter-square fa-fw fa-lg'),
+        ('LinkedIn', 'http://linkedin.com/in/simonorlovsky',
+         'fa fa-linkedin-square fa-fw fa-lg'),
+        ('GitHub', 'http://github.com/sorlovsky',
+         'fa fa-github-square fa-fw fa-lg'),
+        )
+
 
 DEFAULT_PAGINATION = 10
 
